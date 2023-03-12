f32542a54c0f13e6505af5d536a6621015a76b17
Lin Yang <geekya215@gmail.com>
2023-03-12 13:32:39 +0800

    add integer arithmetic testcase

---
+++ src/test/java/TestIntArithmetic.java
@@ -0,0 +1,84 @@
+import io.geekya215.bocchi.Bocchi;
+import io.geekya215.bocchi.Type;
+import io.geekya215.bocchi.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.geekya215.bocchi.Instruction.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestIntArithmetic {
+    private Bocchi bocchi;
+
+    @BeforeEach
+    public void setup() {
+        bocchi = new Bocchi();
+    }
+
+    @Test
+    public void int_add_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IADD.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(8, (int) res.value(), "Return value should be 8");
+    }
+
+    @Test
+    public void int_sub_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), ISUB.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(-2, (int) res.value(), "Return value should be -2");
+    }
+    @Test
+    public void int_mul_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IMUL.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(15, (int) res.value(), "Return value should be 15");
+    }
+    @Test
+    public void int_div_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IDIV.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(0, (int) res.value(), "Return value should be 0");
+    }
+
+    @Test
+    public void int_rem_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IREM.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(3, (int) res.value(), "Return value should be 3");
+    }
+
+    @Test
+    public void int_neg_works() {
+        byte[] buf = {ICONST_5.B(), INEG.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(-5, (int) res.value(), "Return value should be -5");
+    }
+    @Test
+    public void int_and_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IAND.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(1, (int) res.value(), "Return value should be 1");
+    }
+    @Test
+    public void int_or_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IOR.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(7, (int) res.value(), "Return value should be 7");
+    }
+    @Test
+    public void int_xor_works() {
+        byte[] buf = {ICONST_5.B(), ICONST_3.B(), IXOR.B(), IRETURN.B()};
+        Value res = bocchi.eval(buf);
+        assertEquals(Type.I, res.type(), "Return type is int");
+        assertEquals(6, (int) res.value(), "Return value should be 6");
+    }
+}
